328b45708ed893a5b6efcc029fbf260373d681d4
ant_pc <anton-r@live.com>
2020-10-27 23:21:05 +0500

    gg

--- b1/sec.c
+++ b1/sec.c
@@ -7,6 +7,11 @@ int main()
     int i;
 
     printf("enter text:");
+    /* 
+    for reading double :
+    double x;
+    scanf("%lf", &x);
+     */
     scanf("%s %d", str, &i);
 
     printf("\n text is : %s %d \n", str, i);

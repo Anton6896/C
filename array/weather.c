98f3ff538334dd5fe13bb6ff1b22a877ff92f0d2
ant_pc <anton-r@live.com>
2020-11-04 20:03:16 +0500

    d

--- array/weather.c
+++ array/weather.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 
+// glob var 
+#define MONTH 5
+#define YEAR 12
+
 int main()
 {
     float tot_rain_year = 0.0f;
     float average_rain_year = 0.0f;
     float average_rain_month = 0.0f;
 
-    float sol[5][12] = {
+    float sol[MONTH][YEAR] = {
         {3.4,
          2.5,
          3.2,
@@ -77,9 +81,9 @@ int main()
     };
 
     printf("\ntotal rain for year :");
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < MONTH; i++)
     {
-        for (int j = 0; j < 12; j++)
+        for (int j = 0; j < YEAR; j++)
         {
             tot_rain_year += sol[i][j];
         };
@@ -91,9 +95,9 @@ int main()
     printf("\n");
 
     printf("\ntotal rain fall monthly : ");
-    for (int i = 0; i < 12; i++)
+    for (int i = 0; i < YEAR; i++)
     {
-        for (int j = 0; j < 5; j++)
+        for (int j = 0; j < MONTH; j++)
         {
             average_rain_month += sol[j][i];
         };

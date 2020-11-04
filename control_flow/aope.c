c4817025f397056beaac7799e5aebb0def320bcc
Anton.R <Anton-r@ive.com>
2020-11-04 14:01:06 +0500

    dd

--- control_flow/aope.c
+++ control_flow/aope.c
@@ -3,17 +3,23 @@
 #include <stdlib.h>
 #include <time.h>
 
+// guess the number game
 int main() {
-    // guess the number
     printf("guess the number only numbers the 0<num ! (-1 for exit)\n");
-    int guess_me = rand();
+
+    int guess_me;
     int my_guess;
     int count = 0;
     bool in_game = true;
-    // srand((unsigned)time(&guess_me));
+
+    // get random num between 0 - 20
+    time_t t;
+    srand((unsigned)time(&t));
+    guess_me = rand() % 21;
+    // printf("num to guess : %d\n", guess_me);
 
     do {
-        printf("enter number or -1 : ");
+        printf("\nenter number or -1 : ");
         scanf("%d", &my_guess);
         printf("\n");
         count++;
@@ -25,9 +31,9 @@ int main() {
             printf(" tnx for the game .\n ");
             in_game = false;
         } else if (my_guess > guess_me) {
-            printf(" your guess is bigger \n");
+            printf(" your guess is bigger");
         } else if (my_guess < guess_me) {
-            printf(" your guess is lower \n");
+            printf(" your guess is lower");
         }
     } while (in_game);
     return 0;

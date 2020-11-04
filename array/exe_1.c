4ec9f8a4e7207565ac59ceca4fd91b8241a70cd8
Anton.R <Anton-r@ive.com>
2020-11-04 17:18:35 +0500

    ff

--- array/exe_1.c
+++ array/exe_1.c
@@ -4,11 +4,12 @@
 int main() {
     int count = 100;
     int position = 3;
-    int arr[100] = {1, 2, 3};
+    int arr[30] = {1, 2, 3};
 
     for (int i = 5; i < count; i++) {
+        // check if  : (num % i != 0) ? add to list : pass
         int k = -1;
-        for (int j = i; j > 0; j--) {
+        for (int j = i - 1; j > 1; j--) {
             k = i % j;
             if (k == 0) {
                 break;
@@ -21,8 +22,8 @@ int main() {
         };
     };
 
-    for (int i = 0; i < 100; i++) {
-        printf("%d ", arr[i]);
+    for (int i = 0; i < 30; i++) {
+        printf("%d,", arr[i]);
     }
     printf("\n");
 

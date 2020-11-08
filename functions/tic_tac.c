3716b24045e28321e66d5b78cc158c614f8ae300
ant_pc <anton-r@live.com>
2020-11-08 17:28:36 +0500

    dd

--- functions/tic_tac.c
+++ functions/tic_tac.c
@@ -6,7 +6,8 @@
 void show_desk(char desk[3][3]);
 bool winner(int a, int b, char desk[3][3]);
 bool can_move(int a, int b, char desk[3][3]);
-int make_move(char inner_circleer[50], char sine, char desk[3][3], bool out_circle, bool inner_circle);
+int make_move(char inner_circleer[50], char sine, char desk[3][3], bool a, bool b);
+void exit_game(bool a, bool b);
 
 int main()
 {
@@ -14,6 +15,7 @@ int main()
     char p2_sine = 'o';
     char p1_name[50];
     char p2_name[50];
+
     bool out_circle = true;   // bool game flag
     bool inner_circle = true; // start the game sequence ( -1 for exit )
 
@@ -37,27 +39,31 @@ int main()
         };
 
         // get inner_circleer names
-        printf("hello and wellcome to the game tic-toc please use 1-9 keys\n \
-        as at right side of your key board , enjoy");
-        printf("\nplease enter the inner_circleer 1 name he is using the 'X' sine : ");
+        printf("\nhello and wellcome to the game tic-toc please use 1-9 keys as at right side of your key board , enjoy\n\n");
+
+        printf("\nplease enter the 1 name he is using the 'X' sine : ");
         scanf("%s", p1_name);
-        printf("\nplease enter the inner_circleer 2 name he is using the 'O' sine : ");
+
+        printf("\nplease enter the 2 name he is using the 'O' sine : ");
         scanf("%s", p2_name);
+
+        // show board
+        show_desk(board);
 
         while (inner_circle)
         {
-
-            // show board
-            show_desk(board);
-
-            // inner_circleer 1 move
             make_move(p1_name, p1_sine, board, out_circle, inner_circle);
-            // inner_circleer 2 move
             make_move(p2_name, p2_sine, board, out_circle, inner_circle);
         };
     };
     printf("\nTnx for game .\n");
     return 0;
+};
+
+void exit_game(bool a, bool b)
+{
+    a = false;
+    b = false;
 };
 
 void show_desk(char desk[3][3])
@@ -75,6 +81,7 @@ void show_desk(char desk[3][3])
 
 bool winner(int a, int b, char desk[3][3])
 {
+    // todo winner check
     return false;
 };
 
@@ -87,62 +94,61 @@ bool can_move(int a, int b, char desk[3][3])
     return false;
 };
 
-int make_move(char inner_circleer[50], char sine, char desk[3][3], bool out_circle, bool inner_circle)
+int make_move(char inner_circleer[50], char sine, char desk[3][3], bool a, bool b)
 {
     int move = 0;
-    bool now_moving = true;
+    bool turn_loop = true;
 
     do
     {
         do // get inner_circleer move
         {
-
+            if (move == -1)
+            {
+                a = false;
+                b = false;
+                turn_loop = false;
+                printf(" a b set to false ! ");
+                break;
+            };
             printf("%s please enter your move : ", inner_circleer);
             scanf("%d", &move);
 
-
-        } while ((move < 1 || move > 9) && move != -1);
+        } while (move < 1 || move > 9);
 
         // convert move to desk and check if availble
         switch (move)
         {
-        case -1:
-            // exit from all loops and finish the game
-            now_moving = false;
-            out_circle = false;
-            inner_circle = false;
-            break;
-            
         case 1:
             if (can_move(2, 0, desk))
             {
-                now_moving = 0; // move is legal finish move
+                turn_loop = false;
                 desk[2][0] = sine;
                 show_desk(desk);
                 // check for win
             }
             else
             {
-                printf("you cant go ther sorry ! try again");
+                printf("you cant go ther sorry ! try again\n");
             };
             break;
         case 2:
             if (can_move(2, 1, desk))
             {
-                now_moving = 0; // move is legal finish move
+                turn_loop = false;
                 desk[2][1] = sine;
                 show_desk(desk);
                 // check for win
             }
             else
             {
-                printf("you cant go ther sorry ! try again");
+                printf("you cant go ther sorry ! try again\n");
             };
             break;
         case 3:
             if (can_move(2, 2, desk))
             {
-                now_moving = 0; // move is legal finish move
+                turn_loop = false;
                 desk[2][2] = sine;
                 show_desk(desk);
                 // check for win
@@ -156,7 +162,7 @@ int make_move(char inner_circleer[50], char sine, char desk[3][3], bool out_circ
         case 4:
             if (can_move(1, 0, desk))
             {
-                now_moving = 0; // move is legal finish move
+                turn_loop = false;
                 desk[1][0] = sine;
                 show_desk(desk);
                 // check for win
@@ -169,7 +175,7 @@ int make_move(char inner_circleer[50], char sine, char desk[3][3], bool out_circ
         case 5:
             if (can_move(1, 1, desk))
             {
-                now_moving = 0; // move is legal finish move
+                turn_loop = false;
                 desk[1][1] = sine;
                 show_desk(desk);
                 // check for win
@@ -182,7 +188,7 @@ int make_move(char inner_circleer[50], char sine, char desk[3][3], bool out_circ
         case 6:
             if (can_move(1, 2, desk))
             {
-                now_moving = 0; // move is legal finish move
+                turn_loop = false;
                 desk[1][2] = sine;
                 show_desk(desk);
                 // check for win
@@ -196,7 +202,7 @@ int make_move(char inner_circleer[50], char sine, char desk[3][3], bool out_circ
         case 7:
             if (can_move(0, 0, desk))
             {
-                now_moving = 0; // move is legal finish move
+                turn_loop = false;
                 desk[0][0] = sine;
                 show_desk(desk);
                 // check for win
@@ -209,7 +215,7 @@ int make_move(char inner_circleer[50], char sine, char desk[3][3], bool out_circ
         case 8:
             if (can_move(0, 1, desk))
             {
-                now_moving = 0; // move is legal finish move
+                turn_loop = false;
                 desk[0][1] = sine;
                 show_desk(desk);
                 // check for win
@@ -222,7 +228,7 @@ int make_move(char inner_circleer[50], char sine, char desk[3][3], bool out_circ
         case 9:
             if (can_move(0, 2, desk))
             {
-                now_moving = 0; // move is legal finish move
+                turn_loop = false;
                 desk[0][2] = sine;
                 show_desk(desk);
                 // check for win
@@ -237,7 +243,7 @@ int make_move(char inner_circleer[50], char sine, char desk[3][3], bool out_circ
             break;
         }
 
-    } while (now_moving);
+    } while (turn_loop);
 
     return 0;
 };

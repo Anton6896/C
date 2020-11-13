17da48ad22f610a188fbc6c28664a451673e5b59
ant_pc <anton-r@live.com>
2020-11-13 09:32:09 +0500

    Merge https://github.com/Anton6896/C fgdg

--- main.c
+++ main.c
@@ -1,50 +1,9 @@
-<<<<<<< HEAD
 //
 // Created by anton on 09/11/2020.
 //
 
 // #include "char_string/str_learn.c"
-#include "pointers/point.c"
-#include "pointers/mem.c"
-
-
-int main() {
-    //    define_string();
-
-    /*
-    // string and using string functions
-    char text_string[] = {
-            "he C library function double strtod(const char *str, char **endptr)"
-            " converts the string pointed to by the argument str to a floating-poi"};
-    char hello[] = "hello";
-    char world[] = " world";
-    printf("\ncounter is : %d", my_char_counter(text_string));
-    printf("\nconcat 2 chars : %s", my_concat(hello, world));
-
-    char w1[] = "abc";
-    char w2[] = "abcs";
-    printf("\nis they same ? : %d", my_compare(w1, w2));
-*/
-
-    /* using pointers  */
-//    my_pointers();
-//    exe_1();
-//    constant_p();
-//    void_p();
-//    my_main();
-
-    mem();
-
-
-    printf("\n");
-    return 0;
-=======
-//
-// Created by anton on 09/11/2020.
-//
-
-// #include "char_string/str_learn.c"
-#include "pointers/point.c"
+//#include "pointers/point.c"
 #include "pointers/mem.c"
 #include "Struct/my_struct.c"
 
@@ -81,5 +40,4 @@ int main() {
 
     printf("\n");
     return 0;
->>>>>>> 3a9602e146924d8062c81de8e02db24121956aa4
 }
